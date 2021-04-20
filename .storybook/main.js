2417ab31eac5d45f72ea4b32acc9d21040357a2a
Milton Carlos Katoo <mckatoo@gmail.com>
2021-04-20 16:57:15 -0300

    chore: update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss'
+  ],
   babel: async (options) => ({
     ...options,
     plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
