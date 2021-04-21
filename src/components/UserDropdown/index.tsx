44ec048caf518b5f20f70e2eace0437ec946c529
Milton Carlos Katoo <mckatoo@gmail.com>
2021-04-20 23:36:48 -0300

    feat(userdropdown): userdropdown component with test and storybook
    for use in menu

---
+++ src/components/UserDropdown/index.tsx
@@ -0,0 +1,48 @@
+import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown'
+import {
+  AccountCircle,
+  ExitToApp,
+  FavoriteBorder
+} from '@styled-icons/material-outlined'
+import Dropdown from 'components/Dropdown'
+import { signOut } from 'next-auth/client'
+import Link from 'next/link'
+import * as S from './styles'
+
+export type UserDropdownProps = {
+  username: string
+}
+
+const UserDropdown = ({ username }: UserDropdownProps) => (
+  <Dropdown
+    title={
+      <>
+        <AccountCircle size={24} />
+        <S.Username>{username}</S.Username>
+        <ChevronDown size={24} />
+      </>
+    }
+  >
+    <S.Nav>
+      <Link href="/profile/me" passHref>
+        <S.Link>
+          <AccountCircle />
+          <span>My profile</span>
+        </S.Link>
+      </Link>
+      <Link href="/wishlist" passHref>
+        <S.Link title="Wishlist">
+          <FavoriteBorder />
+          <span>Wishlist</span>
+        </S.Link>
+      </Link>
+
+      <S.Link role="button" onClick={() => signOut()} title="Sign out">
+        <ExitToApp />
+        <span>Sign out</span>
+      </S.Link>
+    </S.Nav>
+  </Dropdown>
+)
+
+export default UserDropdown
