41f1c30f977ea137213806895fd6e59e903b74da
Milton Carlos Katoo <mckatoo@gmail.com>
2021-04-20 23:37:44 -0300

    feat(menu): menu component with test and storybook
    main menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,121 @@
+import Button from 'components/Button'
+import Logo from 'components/Logo'
+import MediaMatch from 'components/MediaMatch'
+import UserDropdown from 'components/UserDropdown'
+import Link from 'next/link'
+import { useState } from 'react'
+
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
+import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+
+import * as S from './styles'
+
+export type MenuProps = {
+  username?: string | null
+  loading?: boolean
+}
+
+const Menu = ({ username, loading }: MenuProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
+
+      <S.LogoWrapper>
+        <Link href="/" passHref>
+          <a>
+            <Logo hideOnMobile />
+          </a>
+        </Link>
+      </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
+          <Link href="/explore" passHref>
+            <S.MenuLink>Explore</S.MenuLink>
+          </Link>
+        </S.MenuNav>
+      </MediaMatch>
+
+      {!loading && (
+        <>
+          <S.MenuGroup>
+            <S.IconWrapper>
+              <SearchIcon aria-label="Search" />
+            </S.IconWrapper>
+            {/* <S.IconWrapper>
+              <MediaMatch greaterThan="medium">
+                <NotifyDropdown />
+              </MediaMatch>
+              <MediaMatch lessThan="medium">
+                <Link href="/notify">
+                  <a><NotifyIcon /></a>
+                </Link>
+              </MediaMatch>
+            </S.IconWrapper> */}
+            <MediaMatch greaterThan="medium">
+              {!username ? (
+                <Link href="/sign-in" passHref>
+                  <Button as="a">Sign in</Button>
+                </Link>
+              ) : (
+                <UserDropdown username={username} />
+              )}
+            </MediaMatch>
+          </S.MenuGroup>
+
+          <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+            <CloseIcon
+              aria-label="Close Menu"
+              onClick={() => setIsOpen(false)}
+            />
+            <S.MenuNav>
+              <Link href="/" passHref>
+                <S.MenuLink>Home</S.MenuLink>
+              </Link>
+              <Link href="/games" passHref>
+                <S.MenuLink>Explore</S.MenuLink>
+              </Link>
+
+              {!!username && (
+                <>
+                  <Link href="/profile/me" passHref>
+                    <S.MenuLink>My profile</S.MenuLink>
+                  </Link>
+                  <Link href="/wishlist" passHref>
+                    <S.MenuLink>Wishlist</S.MenuLink>
+                  </Link>
+                </>
+              )}
+            </S.MenuNav>
+
+            {!username && (
+              <S.RegisterBox>
+                <Link href="/sign-in" passHref>
+                  <Button fullWidth size="large" as="a">
+                    Sign in
+                  </Button>
+                </Link>
+                <span>or</span>
+                <Link href="/sign-up" passHref>
+                  <S.CreateAccount title="Sign Up">Sign Up</S.CreateAccount>
+                </Link>
+              </S.RegisterBox>
+            )}
+          </S.MenuFull>
+        </>
+      )}
+    </S.Wrapper>
+  )
+}
+
+export default Menu
---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+import Menu, { MenuProps } from './'
+
+export default {
+  title: 'Menu',
+  component: Menu,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'ikatoo-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<MenuProps> = (args) => <Menu {...args} />
+
+export const Logged: Story<MenuProps> = (args) => <Menu {...args} />
+
+Logged.args = {
+  username: 'mckatoo'
+}
